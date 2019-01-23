f336766806a7dd97d9cca12921f02dbabff78d28
M2D <meskill@qq.com>
2019-01-23 20:17:05 +0800

    Linklist0.01

---
+++ Algorithms/DataStruct/ADT/Linklist/Linklist.md
@@ -0,0 +1,3 @@
+#Linklist
+
+> 
--- XUJCOJ/XUJC1026.c
+++ XUJCOJ/XUJC1026.c
@@ -2,6 +2,18 @@
 
 int main()
 {
-
+    int sum=0,n;
+    int in;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++) 
+    {
+        scanf("%d",&in);
+        sum+=in;
+    }
+    printf("%d",sum/n);
     return 0;
 }
+
+        
+
+

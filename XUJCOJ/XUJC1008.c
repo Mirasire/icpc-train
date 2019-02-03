7e8f486a080f3973b3eec1c19907f46b258508b4
M2D <meskill@qq.com>
2019-02-04 01:11:16 +0800

    normalupdata1

--- XUJCOJ/XUJC1008.c
+++ XUJCOJ/XUJC1008.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
 
 int main()
 {
+    int M,N,D;
+    scanf("%d%d%d",&M,&N,&D);
+    //N is store's number
+    //M is cost in the way to one to another place
     return 0;
 }

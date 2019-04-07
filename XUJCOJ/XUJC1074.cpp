796958d9f89545976e96e8da22af76af11c728b4
Meskill <meskill@qq.com>
2019-04-07 14:55:15 +0800

    XUJCOJ1047

---
+++ XUJCOJ/XUJC1074.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    freopen("in","r",stdin);
+    double n;
+    while(scanf("%lf",&n) != EOF)
+    {
+        printf("%d %.0lf\n",0,n);
+    }
+    return 0;
+}

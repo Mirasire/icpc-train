18b6df974f230dbc3d5c6b3f4f71213b89d4c6b5
Meskill <meskill@qq.com>
2019-04-17 12:20:32 +0800

    XUJC1023-dead

--- XUJCOJ/XUJC1023.cpp
+++ XUJCOJ/XUJC1023.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <limits.h>
 #include <queue>
 #include <algorithm>
 using namespace std;
 
-struct Node;
-void FindWay(Node start);
+const int maxn = 15;
+bool visite[maxn][maxn] = {0};
+struct Node
+{
+    int x,y;
+    int sum:
+}
+const int Max = INT_MIN;

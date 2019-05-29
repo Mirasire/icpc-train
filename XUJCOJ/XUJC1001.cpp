e3abfc957d62f44bc04b6119a67096d5dbe1be8d
meskill <meskill@qq.com>
2019-05-29 22:26:52 +0800

    XUJC1001.cpp_0.0

---
+++ XUJCOJ/XUJC1001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <set>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int maxn = 1e2+10;
+int arr[maxn];
+
+typedef struct BinNode * PBinNode;
+
+struct BinNode{
+	PBinNode
+};
+
+int main(){
+	int N;
+	scanf("%d",&N);
+	for(int i=0; i<N; i++) scanf("%d",arr+i);
+	return 0;
+}

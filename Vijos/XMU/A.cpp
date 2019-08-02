efe644c0f396dc49707ee9fc0ba026bbe4ba1098
Meskill <meskill@qq.com>
2019-08-02 14:40:10 +0800

    GHDHH1

---
+++ Vijos/XMU/A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll a,p,b,x=0;
+    ll tmp;
+    freopen("in","r",stdin);
+    while(scanf("%lld%lld%lld",&a,&p,&b) == 3) {
+        if(!(a%p) || !(p%a)) printf("daishixiongzuishuai\n");
+        else {
+            x = 0;
+            tmp = 1;
+            while(1) {
+                if(!((int)abs(tmp - b) % p)) break;
+                x++;
+                tmp = tmp*a;
+            }
+            printf("%lld\n",x);
+        }
+    }
+    return 0;
+}
+
+
+            
+

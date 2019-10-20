a837a03d45da80f03fb6c5454f1d5d2320df64b4
meskill <meskill@qq.com>
2019-10-20 13:55:57 +0800

    Hexo&&Github0.01

--- Life In OJ/OJ's Solution/POJ/POJ3436.cpp
+++ Life In OJ/OJ's Solution/POJ/POJ3436.cpp
@@ -3,146 +3,169 @@
 #include <cstdio>
 #include <cstring>
 #include <string>
-#include <vector>
 #include <queue>
 using namespace std;
 
-const int Hmax = 80,Pmax = 13,INF = 0x3f3f3f3f;
-int cnt = -1;
-int head[Hmax<<1],cur[Hmax<<1],dis[Hmax<<1];
-//head -> -1
-int s,t,N,P;
+//Dinic Init
+/* DebugList
+ * cout << now << " --> " << e.to << "ff: " << e.cap - e.flow << " mff: " << mff << " t: " << t << endl;
 
-struct MACHINE {
-	int mk;
-	int in[Pmax],out[Pmax];
-} mac[Hmax];
+	for(int j=0; j<=n*2+1; j++)
+		for(int i=dinic.head[j]; i!=-1; i=dinic.edge[i].next) {
+			cout << j << "-->" << dinic.edge[i].to << endl;
+		}
+ */
 
+const int Pmaxn = 70;
+const int MaxNode = Pmaxn*Pmaxn;
+const int inf = 0x3f3f3f3f;
+int p,n;
+int out[Pmaxn][3];
 struct EDGE {
 	int to,next,flow,cap,rev;
-	EDGE() {}
-	EDGE(int to,int next,int flow,int cap,int rev):to(to),next(next),flow(flow),cap(cap),rev(rev) {}
-	void init(int to,int next,int flow,int cap) {
-		this->to = to;
-		this->next = next;
-		this->flow = flow;
-		this->cap = cap;
-		this->rev = rev;
+	EDGE(){}
+	void init(int to,int next,int flow,int cap,int rev) {
+		this->to    = to;
+		this->next  = next;
+		this->flow  = flow;
+		this->cap   = cap;
+		this->rev   = rev;
 	}
-} edge[Hmax<<1];
+};
 
-void addEdge(int a,int b,int cap) {
-	edge[++cnt].init(b,head[a],0,cap,cnt^1); head[a]=cnt;
-	edge[++cnt].init(a,head[b],0,0,cnt^1); head[b]=cnt;
-}
-
-bool bfs() {
-	//init the distance
-	memset(dis,-1,sizeof(dis));
-	int now,i;
-	queue<int> list;
-	list.push(s);
-	dis[i] = 0;
-	while(!list.empty()) {
-		now = list.front();
-		list.pop();
-		for( i=head[now]; i!=-1; i=edge[i].next) {
-			EDGE& e = edge[i];
-			if(dis[e.to] != -1 && e.cap > e.flow) {
-				dis[e.to] = dis[now] + 1;
-				list.push(e.to);
-			}
-		}
+struct DINIC {
+	EDGE edge[MaxNode];
+	int cnt,s,t;
+	int dis[MaxNode],head[MaxNode],cur[MaxNode],vis[MaxNode];
+	DINIC() {
+		memset(head,-1,sizeof(head));
+		cnt=-1;
 	}
-	return dis[t];
-}
-
-int dfs(int now,int mff) {
-	if(now==t || mff==0) return mff;
-	int flow = 0, f;
-	for(int& i=cur[now]; i!=-1; i=edge[i].to) { //cur showed be init to the head[i]
-		EDGE& e = edge[i];
-		if(dis[e.to] == dis[now] + 1 && (f = dfs(e.to,min(mff,e.cap-e.flow))) > 0) {
-			e.flow += f;
-			edge[e.rev].flow -= f;
-			flow += f;
-			mff -= f;
-			if(mff == 0) break;
-		}
+	void addEdge(int a,int b,int cap) {
+		edge[++cnt].init(b,head[a],0,cap,cnt^1); head[a] = cnt;
+		edge[++cnt].init(a,head[b],0,0,cnt^1); head[b] = cnt;
 	}
-	return flow;
-}
-
-int getMaxFlow(const int size) {
-	int mflow = 0,i;
-	while(bfs()) {
-		for(i=0; i<size; i++) cur[i] = head[i];
-		mflow+=dfs();
-	}
-	return mflow;
-}
-
-void perdo() {
-	bool flag;
-	for(int i=0; i<N; i++) {
-		flag = 1;
-		for(int j=0; j<P; j++) 
-			if(mac[i].in[j]&1) {
-				flag = 0;
-				break;
-			}
-		if(flag) addEdge(s,i,INF);
-	}
-	for(int i=0; i<N; i++) 
-		addEdge(i,i+N,mac[i].mk);
-
-	for(int i=0; i<N; i++) {
-		for(int j=0; j<N; j++) {
-			if(i==j) continue;
-			flag = 1;
-			for(int k=0; k<p; k++) {
-				if(mac[i].out[k] != mac[j].in[k] && mac[j].in[k] != 2) {
-					flag = 0;
-					breakl
+	bool bfs() {
+		memset(vis,0,sizeof(vis));
+		int now,i;
+		queue<int> list;
+		list.push(s);
+		dis[s] = 0;
+		vis[s] = 1;
+		while(!list.empty()) {
+			now = list.front();
+			list.pop();
+			for(i=head[now]; i!=-1; i=edge[i].next) {
+				EDGE& e = edge[i];
+				if(!vis[e.to] && e.cap > e.flow) {
+					vis[e.to] = 1;
+					dis[e.to] = dis[now] + 1;
+					list.push(e.to);
 				}
 			}
-			if(flag) addEdge(i+N,j,INF);
 		}
+		return vis[t];
 	}
-
-	for(int i=0; i<N; i++) {
-		flag = 1;
-		for(int j=0; j<P; j++) {
-			if(!mac[i].out[j]) {
-				flag = 0;
-				break;
+	int dfs(int now,int mff) {
+		if(now == t || mff == 0) return mff;
+		int f,flow = 0;
+		for(int& i = cur[now]; i!=-1; i=edge[i].next) {
+			EDGE& e = edge[i];
+			f = dfs(e.to,min(mff,e.cap - e.flow));
+			if(dis[e.to] == (dis[now] + 1) && f > 0) {
+				e.flow += f;
+				edge[e.rev].flow -= f;
+				flow += f;
+				mff -= f;
+				if(mff==0) break;
 			}
 		}
-		if(flag) addEdge(i+N,t,INF);
+		return flow;
 	}
-}
-
-void res() {
-	int maxFlow = getMaxFlow();
-}
-
-void slove() {
-	int i,j;
-	scanf("%d%d",&P,&N);
-	s = N*2+52;
-	s = N*2+53;
-	for(i=0; i<N; i++) {
-		scanf("%d",&mac[i].mk);
-		for(j=0; j<P; j++)
-			scanf("%d",&mac[i].in[j]);
-		for(j=0; j<P; j++)
-			scanf("%d",&mac[i].out[j]);
+	int getMaxFlow(int s,int t) {
+		int flow = 0;
+		this->s = s;
+		this->t = t;
+		
+		while(bfs()) {
+			for(int i=0; i<=n*2+1; i++) cur[i] = head[i];
+			flow += dfs(s,inf);
+		}
+		return flow;
 	}
-	perdo();
-	res();
-}
+} dinic;
+
+//for save
+struct INPUT {
+	int val,inp[13],out[13];
+} inpt[60];
+
+void slove(int s,int t);
 
 int main() {
-	slove();
+	int s,t;
+	scanf("%d%d",&p,&n);
+	for(int i=1; i<=n; i++) {
+		scanf("%d",&inpt[i].val);
+		for(int j=0; j<p; j++) scanf("%d",&inpt[i].inp[j]);
+		for(int j=0; j<p; j++) scanf("%d",&inpt[i].out[j]);
+	}
+	//perdu
+	
+	//link to the source
+	s = 0;
+	t = n*2+1;
+	for(int i=1; i<=n; i++) {
+		bool flag=1;
+		for(int j=0; j<p&&flag; j++) {
+			if(inpt[i].inp[j]&1) flag=0;
+		}
+		if(flag) dinic.addEdge(s,i,inf);
+	}
+
+	//拆点
+	for(int i=1; i<=n; i++) dinic.addEdge(i,i+n,inpt[i].val);
+
+	for(int u=1; u<=n; u++) {
+		for(int v=1; v<=n; v++) {
+			if(u==v) continue;
+			bool flag = 1;
+			for(int i=0; i<p&&flag; i++) {
+				if(inpt[u].out[i] != inpt[v].inp[i] && inpt[v].inp[i] != 2) flag = 0;
+			}
+			if(flag) dinic.addEdge(u+n,v,inf);
+		}
+	}
+
+	for(int i=1; i<=n; i++) {
+		bool flag=1;
+		for(int j=0; j<p&&flag; j++) {
+			if(inpt[i].out[j]!=1) flag=0;
+		}
+		if(flag) dinic.addEdge(i+n,t,inf);
+	}
+
+	slove(s,t);
 	return 0;
+}
+
+void slove(int s,int t) {
+	int maxFlow = dinic.getMaxFlow(s,t);
+	int ctt = 0;
+	int end = n*n;
+	for(int i=n+1; i<=end; i++) {
+		for(int j = dinic.head[i]; j!=-1; j=dinic.edge[j].next) {
+			EDGE& e = dinic.edge[j];
+			//cout << i << " --> " << e.to << " flow: " << e.flow << endl;
+			if(e.cap && e.flow && e.to <= n) {
+				out[ctt][0] = e.flow;
+				out[ctt][1] = i-n;
+				out[ctt++][2] = e.to;
+			}
+		}
+	}
+	printf("%d %d\n",maxFlow,ctt);
+	for(int i=0; i<ctt; i++) {
+		printf("%d %d %d\n",out[i][1],out[i][2],out[i][0]);
+	}
 }

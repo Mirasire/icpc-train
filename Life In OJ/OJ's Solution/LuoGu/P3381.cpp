fb50627c0292ebfc52feba158a87506e43193126
meskill <meskill@qq.com>
2019-11-06 23:53:40 +0800

    P3381-oiwiki-100

--- Life In OJ/OJ's Solution/LuoGu/P3381.cpp
+++ Life In OJ/OJ's Solution/LuoGu/P3381.cpp
@@ -1,111 +1,67 @@
-//Memory Limited 125.00MB
-#include <iostream>
+//OI-Wiki solution
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <string>
 #include <queue>
-#include <vector>
-using namespace std;
 
-typedef long long ll;
-typedef unsigned short int usl;
-const int Vm = 5e3+5,Em = 1e5+4,INF = 0x3f3f3f;
+const int N = 5e3 + 5, M = 1e5 + 5;
+const int INF = 0x3f3f3f3f;
+int n, m, tot = 1, lnk[N], cur[N], ter[M], nxt[M], cap[M], cost[M], dis[N], ret;
+bool vis[N];
 
-struct EDGE {
-	int to,next,flow,cap,cost;
-	inline void init(int to,int next,int cap,int cost) {
-		this->flow = 0;
-		this->to = to;
-		this->next = next;
-		this->cap = cap;
-		this->cost = cost;
-	}
-};
-
-struct MCFC {
-	int head[Vm],cur[Vm],dist[Vm];
-	int s,t,cnt,maxFlow;
-	int minCost;
-	bool vis[Vm];
-	EDGE edge[Em];
-	MCFC() {
-		memset(head,-1,sizeof(head));
-		cnt = -1;
-	}
-
-	void AddEdge(int a,int b,int cap,int cost) {
-		edge[++cnt].init(b,head[a],cap,cost); head[a] = cnt;
-		edge[++cnt].init(a,head[b],0,-cost); head[b] = cnt;
-	}
-
-	bool spfa() {
-		//init
-		int now,i;
-		fill(dist,dist+Vm,INF);
-		memset(vis,0,sizeof(vis));
-		memcpy(cur,head,sizeof(head)); //maybe for dfs?
-
-		queue<int> list;
-		list.push(s);
-		vis[s] = 1;
-		dist[s] = 0;
-		while(!list.empty()) {
-			now = list.front();
-			list.pop();
-			vis[now] = 0;
-			//cout << "dist(now) " << dist[now] << endl;
-			for(i = head[now]; i!=-1; i=edge[i].next) {
-				//cout << "i " << i << endl;
-				EDGE& e = edge[i];
-				if(e.cap > e.flow && dist[e.to] > dist[now] + e.cost) {
-					dist[e.to] = dist[now] + e.cost;
-					if(!vis[e.to]) vis[e.to]=1,list.push(e.to);
-				}
-			}
-		}
-		return dist[t]!=INF;
-	}
-
-	int dfs(int u,int f) {
-		if(u == t || f == 0) return f;
-		//vis[u] = 1;
-		int ff = 0,rf = 0;
-		for(int& i=cur[u]; i!=-1; i=edge[i].next) {
-			EDGE& e = edge[i];
-			if(/*!vis[e.to] &&*/ (dist[e.to] == dist[u] + e.cost) && (ff = dfs(e.to,min(f,e.cap-e.flow))) > 0) {
-				minCost += ff*e.cost; //计算费用
-				//普通最大流操作
-				e.flow += ff;	
-				edge[i^1].flow -= ff;
-				rf += ff;
-				f -= ff;
-				if(!f) break;
-			}
-		}
-		//vis[u] = 0;
-		return rf;
-	}
-
-	void mcfc(int s,int t) {
-		minCost = maxFlow = 0;
-		this->s = s; this->t = t;
-		while(spfa()) maxFlow += dfs(s,INF);
-		return;
-	}
-};
-
+void add(int u, int v, int w, int c) {
+  ter[++tot] = v, nxt[tot] = lnk[u], lnk[u] = tot, cap[tot] = w, cost[tot] = c;
+}
+void addedge(int u, int v, int w, int c) { add(u, v, w, c), add(v, u, 0, -c); }
+bool spfa(int s, int t) {
+  memset(dis, 0x3f, sizeof(dis));
+  memcpy(cur, lnk, sizeof(lnk));
+  std::queue<int> q;
+  q.push(s), dis[s] = 0, vis[s] = 1;
+  while (!q.empty()) {
+    int u = q.front();
+    q.pop(), vis[u] = 0;
+    for (int i = lnk[u]; i; i = nxt[i]) {
+      int v = ter[i];
+      if (cap[i] && dis[v] > dis[u] + cost[i]) {
+        dis[v] = dis[u] + cost[i];
+        if (!vis[v]) q.push(v), vis[v] = 1;
+      }
+    }
+  }
+  return dis[t] != INF;
+}
+int dfs(int u, int t, int flow) {
+  if (u == t) return flow;
+  vis[u] = 1;
+  int ans = 0;
+  for (int &i = cur[u]; i && ans < flow; i = nxt[i]) {
+    int v = ter[i];
+    if (!vis[v] && cap[i] && dis[v] == dis[u] + cost[i]) {
+      int x = dfs(v, t, std::min(cap[i], flow - ans));
+      if (x) ret += x * cost[i], cap[i] -= x, cap[i ^ 1] += x, ans += x;
+    }
+  }
+  vis[u] = 0;
+  return ans;
+}
+int mcmf(int s, int t) {
+  int ans = 0;
+  while (spfa(s, t)) {
+    int x;
+    while ((x = dfs(s, t, INF))) ans += x;
+  }
+  return ans;
+}
 int main() {
-	cout << sizeof(MCFC) << endl;
-	MCFC dinic;
-	int n,m,s,t;
-	int a,b,c,co;
-	scanf("%d%d%d%d",&n,&m,&s,&t);
-	while(m--) {
-		scanf("%d%d%d%d",&a,&b,&c,&co);
-		dinic.AddEdge(a,b,c,co);
-	}
-	dinic.mcfc(s,t);
-	printf("%d %d\n",dinic.maxFlow,dinic.minCost);
-	return 0;
+  int s, t;
+  scanf("%d%d%d%d", &n, &m, &s, &t);
+  while (m--) {
+    int u, v, w, c;
+    scanf("%d%d%d%d", &u, &v, &w, &c);
+    addedge(u, v, w, c);
+  }
+  int ans = mcmf(s, t);
+  printf("%d %d\n", ans, ret);
+  return 0;
 }

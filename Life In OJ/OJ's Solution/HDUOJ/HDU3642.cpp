2d32eec8b284ba3851e3ea8a7ace29c70f55f67a
Mirasire <meskill@qq.com>
2019-11-15 16:57:10 +0800

    HDU3642

---
+++ Life In OJ/OJ's Solution/HDUOJ/HDU3642.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <string>
+using namespace std;
+
+const int maxn = 1e3+100;
+struct {
+
+}
+
+int main() {
+	return 0;
+}

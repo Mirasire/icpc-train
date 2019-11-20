e7598dd5272cbdc4046889c41ef35ac413c84618
meskill <meskill@qq.com>
2019-11-20 13:28:16 +0800

    601C&B

--- Life In OJ/OJ's Solution/CodeForces/Round#601/B.cpp
+++ Life In OJ/OJ's Solution/CodeForces/Round#601/B.cpp
@@ -4,50 +4,46 @@
 #include <cstring>
 #include <string>
 using namespace std;
-
-const long long int maxn = 1e3+100;
+ 
+const int maxn = 1e3+100;
 struct NODE {
-	long long int num;
-	long long int pos;
+	int num;
+	int pos;
 } a[maxn];
-
+ 
 bool cmp(NODE& a,NODE &b) {
 	return a.num < b.num;
 }
-
+ 
 int main() {
-	long long int t,n,m;
-	long long int ans = 0;
-	scanf("%lld",&t);
+	int t,n,m;
+	int ans = 0;
+	scanf("%d",&t);
 	while(t--) {
 		ans = 0;
-		scanf("%lld%lld",&n,&m);
-		for(long long int i=0; i<n; i++) {
-			scanf("%lld",&a[i].num);
+		scanf("%d%d",&n,&m);
+		for(int i=0; i<n; i++) {
+			scanf("%d",&a[i].num);
 			a[i].pos = i+1;
+			ans += a[i].num;
 		}
-
-		if(n==2) {
-			ans = a[0].num+a[1].num;
-			printf("%lld\n",ans*m);
-			for(long long int i=0; i<m; i++) printf("1 2\n");
-		}
-		else if(m<(n-1)) {
+		ans <<= 1;
+		if(m<n || n==2) {
 			printf("-1\n");
 			continue;
 		} else {
 			sort(a,a+n,cmp);
-			for(int i=1; i<n; i++) {
-				ans += a[0].num + a[i].num;
+			ans += (a[0].num+a[1].num)*(m-n);
+			printf("%d\n",ans);
+			int end = m-n;
+			for(int i=1; i<=n; i++) {
+				if(i!=n) 
+					printf("%d %d\n",i,(i+1));
+				else 
+					printf("%d %d\n",i,1);
 			}
-			long long int end = m-n+1;
-			ans += (a[0].num + a[1].num)*end;
-			printf("%lld\n",ans);
-			for(long long int i=1; i<n; i++) {
-					printf("%lld %lld\n",a[0].pos,a[i].pos);
-			}
-			for(long long int i=0; i<end; i++) {
-				printf("%lld %lld\n",a[0].pos,a[1].pos);
+			for(int i=0; i<end; i++) {
+				printf("%d %d\n",a[0].pos,a[1].pos);
 			}
 		}
 	}
--- Life In OJ/OJ's Solution/CodeForces/Round#601/C.cpp
+++ Life In OJ/OJ's Solution/CodeForces/Round#601/C.cpp
@@ -3,8 +3,40 @@
 #include <cstdio>
 #include <cstring>
 #include <string>
+#include <vector>
 using namespace std;
 
+const int maxn=1e5+100;
+vector<int> a[maxn];
+bool vis[maxn];
+int cnt[maxn];
+
 int main() {
+	int n;
+	int x,y,z;
+	scanf("%d",&n);
+	for(int i=0; i<n-2; i++) {
+		scanf("%d%d%d",&x,&y,&z);
+		a[x].push_back(y);a[x].push_back(z);
+		a[y].push_back(x);a[y].push_back(z);
+		a[z].push_back(x);a[z].push_back(y);
+		cnt[x]++;cnt[y]++;cnt[z]++;
+	}
+	for(int i=1; i<=n; i++)  {
+		if(cnt[i]==1) 	x = i;
+	}
+	if(cnt[a[x][0]] == 2)  y=a[x][0];
+	else y=a[x][1];
+	printf("%d %d",x,y);
+	vis[x] = vis[y] = 1;
+	for(int i=n-2; i>0; i--) {
+		for(int j=0; j<a[x].size(); j++) {
+			if(!vis[a[x][j]]) z = a[x][j];
+		}
+		vis[z] = 1;
+		printf(" %d",z);
+		x = y;
+		y = z;
+	}
 	return 0;
 }

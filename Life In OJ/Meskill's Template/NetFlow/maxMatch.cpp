acd11f2bd510a179a376a846653c91e72fac5613
Mirasire <meskill@qq.com>
2019-11-26 17:16:52 +0800

    bestMatch

--- Life In OJ/Meskill's Template/NetFlow/maxMatch.cpp
+++ Life In OJ/Meskill's Template/NetFlow/maxMatch.cpp
@@ -2,7 +2,7 @@
 //n为Y集,base1
 bool dfs(int u) {
 	for(int v=1; v=n; v++) {
-		if(line[u][v] && !vis[v]) {
+		if(line[u][v] && !vis[v]) { //!!
 			vis[v] = 1;
 			if(match[v]==-1 || dfs(match[v])) {
 				match[v] = u;
@@ -18,6 +18,6 @@ int sum = 0; //记录匹配数目
 memset(match,-1,sizeof(match)); //初始化Y匹配的X的编号
 
 for(int i=1; i<=n; i++) {
-	memset(vis,0,sizeof(vis));
+	memset(vis,0,sizeof(vis)); //!!
 	if(dfs(i)) sum++;
 }

a4d7f9d8a5e9f988edec5d839ca87300c782f93e
meskill <meskill@qq.com>
2019-11-29 21:17:05 +0800

    ERC

--- Life In OJ/OJ's Solution/CodeForces/EducationRound#77/C.cpp
+++ Life In OJ/OJ's Solution/CodeForces/EducationRound#77/C.cpp
@@ -1,43 +1,19 @@
-//wei AC
 #include <iostream>
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
 #include <string>
-#include <map>
-#include <cmath>
-#include <limits.h>
 using namespace std;
 
-typedef long long int ll;
-
 int main() {
-	int n;
-	ll r,b,k,mxx;
-	scanf("%d",&n);
-	while(n--) {
-		ll maxMod = INT_MIN;
-		scanf("%lld%lld%lld",&r,&b,&k);
-		map<int,bool> app;
+	int t,r,b,k,gcd;
+	scanf("%d",&t);
+	while(t--) {
+		scanf("%d%d%d",&r,&b,&k);
 		if(r>b) swap(r,b);
-		mxx = (b-1)/r;
-		if(r==b) {
-			printf("OBEY\n");
-			continue;
-		}
-		ll mod = b%r;
-		ll end = sqrt(mod)+100;
-		for(ll i=0; i<=end; i++) {
-			app[mod] = 1;
-			mxx = max(mxx,(b+mod-1)/r);
-			mod = (b+mod)%r;
-			if(app[mod] || mod < maxMod) break;
-			maxMod = max(maxMod,mod);
-		}
-		if(mxx < k) 
-			printf("OBEY\n");
-		else 
-			printf("REBEL\n");
+		gcd = __gcd(r,b);
+		if((b-1-gcd)/r+1 >= k) puts("REBEL");
+		else puts("OBEY");
 	}
 	return 0;
 }
--- Life In OJ/OJ's Solution/HDUOJ/HDU2732.cpp
+++ Life In OJ/OJ's Solution/HDUOJ/HDU2732.cpp
@@ -8,7 +8,7 @@
 #include <queue>
 using namespace std;
 
-#define maxn 25
+const int maxn = 1e3+100;
 #define INF 0x3f3f3f3f
 
 struct PILLER {
@@ -17,7 +17,7 @@ struct PILLER {
 	PILLER() {}
 };
 vector<PILLER> pList;
-char liz[maxn][maxn],liz[maxn][maxn];
+char liz[maxn][maxn],pil[maxn][maxn];
 int idx[maxn][maxn],cap[maxn][maxn];
 
 
@@ -25,16 +25,19 @@ struct Edge {
 	int from, to, cap, flow;
 	Edge(int u, int v, int c, int f) : from(u), to(v), cap(c), flow(f) {}
 };
+
 struct Dinic {
 	int n, m, s, t;
 	vector<Edge> edges;
 	vector<int> G[maxn];
 	int d[maxn], cur[maxn];
 	bool vis[maxn];
+
 	void init(int n) {
 		for (int i = 0; i < n; i++) G[i].clear();
 		edges.clear();
 	}
+
 	void AddEdge(int from, int to, int cap) {
 		edges.push_back(Edge(from, to, cap, 0));
 		edges.push_back(Edge(to, from, 0, 0));
@@ -42,12 +45,13 @@ struct Dinic {
 		G[from].push_back(m - 2);
 		G[to].push_back(m - 1);
 	}
+
 	bool BFS() {
 		memset(vis, 0, sizeof(vis));
 		queue<int> Q;
 		Q.push(s);
 		d[s] = 0;
-		vis[s] = 2;
+		vis[s] = 1;
 		while (!Q.empty()) {
 			int x = Q.front();
 			Q.pop();
@@ -56,12 +60,13 @@ struct Dinic {
 				if (!vis[e.to] && e.cap > e.flow) {
 					vis[e.to] = 1;
 					d[e.to] = d[x] + 1;
-					q.push(e.to);
+					Q.push(e.to);
 				}
 			}
 		}
 		return vis[t];
 	}
+
 	int DFS(int x, int a) {
 		if (x == t || a == 0) return a;
 		int flow = 0, f;
@@ -77,6 +82,7 @@ struct Dinic {
 		}
 		return flow;
 	}
+
 	int Maxflow(int s, int t) {
 		this->s = s;
 		this->t = t;
@@ -90,39 +96,51 @@ struct Dinic {
 };
 
 int main() {
+	int kase = 0;
 	Dinic dinic;
 	int T,mini,dis;
+	int lizCnt;
 	int s,t,psize;
 	int n,m,d,cnt;
 	scanf("%d",&T);
 	while(T--) {
-		m = cnt = 0;
+		//INIT
+		memset(liz,0,sizeof(liz));
+		memset(pil,0,sizeof(pil));
+		memset(idx,0,sizeof(idx));
+		memset(cap,0,sizeof(idx));
+		pList.clear();
+		lizCnt = m = cnt = 0;
+
 		scanf("%d%d",&n,&d);
 		//cin the pillers
 		for(int i=0; i<n; i++) {
 			scanf(" %s",pil[i]);
+			//printf("%s\n",pil[i]);
 			if(!m) m = strlen(pil[i]);
 			for(int j=0; j<m; j++) {
 				if(pil[i][j] != '0') {
-					idx[i][j] = ++idx;
+					idx[i][j] = ++cnt;
 					cap[i][j] = pil[i][j] - '0';
-					pList.push_back((i,j));
+					pList.push_back({i,j});
 				}
 			}
 		}
 
 		//set the s and t && psize
 		s = 0;
-		t = cnt<<1|1;
+		t = (cnt<<1) + 100;
         dinic.init(t+1);
 		psize = pList.size();
 
 		//cin the lizards
 		for(int i=0; i<n; i++) {
 			scanf(" %s",liz[i]);
+			//printf("%s\n",liz[i]);
 			for(int j=0; j<m; j++) {
 				if(liz[i][j] == 'L') {
-					dinic.AddEdge(s,idx[i][j],cap[i][j],1);
+					lizCnt++;
+					dinic.AddEdge(s,idx[i][j],1);
 				}
 			}
 		}
@@ -130,10 +148,14 @@ int main() {
 		//cut point
 		for(int i=0; i<psize; i++) {
 			PILLER& now = pList[i];
-			dinic.AddEdge(idx[now.x][now.y],idx[now.x][now.y]+cnt,cap[i][j]);
+			dinic.AddEdge(idx[now.x][now.y],idx[now.x][now.y]+cnt,cap[now.x][now.y]);
 			mini = min(min(now.x+1,n-now.x),min(now.y+1,m-now.y));
-			if(mini <= d) dinic.AddEdge(idx[now.x][now.y]+cnt,t,INF);
+			if(mini <= d) {
+				//cout << "I add < " << now.x+1 << "," << n-now.x << " >" << endl;
+				dinic.AddEdge(idx[now.x][now.y]+cnt,t,INF);
+			}
 		}
+
 
 		//make them connect
 		for(int i=0; i<psize; i++) {
@@ -143,11 +165,17 @@ int main() {
 				dis = abs(now.x-to.x) + abs(now.y-to.y);
 				if(dis <= d) {
 					dinic.AddEdge(idx[now.x][now.y]+cnt,idx[to.x][to.y],INF);
-					dinic.AddEdge(idx[tt.x][tt.y]+cnt,idx[now.x][now.y],INF);
+					dinic.AddEdge(idx[to.x][to.y]+cnt,idx[now.x][now.y],INF);
 				}
 			}
 		}
-        printf("%d\n",dinic.Maxflow(s,t));
+		int mflow = dinic.Maxflow(s,t);
+		//cout << "MaxFlow is -> " << mflow << endl;
+		int left = lizCnt - mflow;
+		if(left==1) printf("Case #%d: 1 lizard was left behind.\n",++kase);
+		else if(left) printf("Case #%d: %d lizards were left behind.\n",++kase,left);
+		else printf("Case #%d: no lizard was left behind.\n",++kase);
 	}
     return 0;
 }
+

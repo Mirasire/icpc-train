241e55de78497188694c878d862b80ab55214c28
meskill <meskill@qq.com>
2019-12-04 07:39:58 +0800

    HDU3081-bug1

--- Life In OJ/OJ's Solution/HDUOJ/HDU3081.cpp
+++ Life In OJ/OJ's Solution/HDUOJ/HDU3081.cpp
@@ -6,6 +6,107 @@
 using namespace std;
 
 
+//Let boy 1+n-n<<1 gril-1-n
+const int maxn = 230;
+int n,m,f; //people m-<b,g> f-<g,g>
+int fa[maxn],grp[maxn][maxn],match[maxn];
+bool vis[maxn];
+
+int findF(int u) {
+	int tmp = u,ans;
+	while(fa[tmp] != tmp) { tmp = fa[tmp]; }
+	ans = tmp;
+	while(fa[u] != ans) {
+		tmp = fa[u];
+		fa[u] = ans;
+		u = tmp;
+	}
+	return ans;
+}
+
+void unite(int a,int b) {
+	int ra = findF(a);
+	int rb = findF(b);
+	if(ra==rb) return;
+	else fa[ra] = rb;
+}
+
+bool isMatch(int u) {
+	int end = n<<1;
+	for(int v=n+1; v<=end; v++) {
+		if(!vis[v]) {
+			vis[v] = 1;
+			if(grp[u][v] || isMatch(match[v])) {
+				match[v] = u;
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+bool xyl() {
+	int check = 0;
+	for(int i=1; i<=n; i++) {
+		memset(vis,0,sizeof(vis));
+		check += isMatch(i);
+	}
+	return(check==n);
+}
+
+//make grile connet to boy
+void buildGraph() {
+	int end = n<<1;
+	for(int i=n+1; i<=end; i++) {
+		for(int j=1; j<=n; j++) {
+			if(grp[j][i]) {
+				for(int k=1; k<=n; k++) {
+					if(findF(j) == findF(k)) {
+						cout << "grils to grils = " << j << " " << k << endl;
+						cout << "boy to grils = " << (i-4) << " " << k << endl;
+						grp[i][k] = grp[k][i] = 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+int slove() {
+	int ans = 0;
+	while(xyl()) {
+		ans++;
+		for(int i=n+1; i<=(n*2); i++) grp[i][match[i]] = grp[match[i]][i] = 0;
+	}
+	return ans;
+}
+
 int main() {
+	int t;
+	int b,g;
+	int g1,g2;
+	scanf("%d",&t);
+	while(t--) {
+		//don't forget init
+		scanf("%d%d%d",&n,&m,&f);
+		for(int i=0; i<=(n<<1); i++) fa[i] = i;
+
+		for(int i=0; i<m; i++) {
+			scanf("%d%d",&g,&b);
+			b+=n;
+			grp[g][b] = grp[b][g] = 1;
+		}
+		for(int i=0; i<f; i++) {
+			scanf("%d%d",&g1,&g2);
+			unite(g1,g2);
+		}
+		//OK make sure the union set is OK
+		buildGraph();
+		for(int i=1; i<=n; i++)
+			for(int j=n+1; j<=(n*2); j++) {
+				printf("%d%c",grp[i][j]," \n"[j==(n<<1)]);
+			}
+		printf("%d\n",slove());
+	}
 	return 0;
 }

fdaf80d088dcace48d4df1c4b063972063f05402
meskill <meskill@qq.com>
2019-12-06 09:46:51 +0800

    P2855

--- Life In OJ/OJ's Solution/LuoGu/P2855.cpp
+++ Life In OJ/OJ's Solution/LuoGu/P2855.cpp
@@ -1,8 +1,45 @@
+//AC ^.^
 #include <bits/stdc++.h>
 using namespace std;
 
-//TODO
+const int maxn = 5e4+100;
+int arr[maxn],diff[maxn],l,n,m;
+
+bool check(int val) {
+	int sum = diff[0],cnt = 0;
+	for(int i=1; i<=n; i++) {
+		if(sum >= val) {
+			sum = diff[i];
+		}
+		else if(sum < val) {
+			if(cnt < m) {
+				sum += diff[i];
+				cnt++;
+			} else {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+int binary_search(int MM) { //imp
+	int l=0,r=MM+1,mid;
+	while(l+1<r) {
+		mid = (l+r)>>1;
+		if(check(mid)) l = mid;
+		else r = mid;
+	}
+	return l;
+}
 
 int main() {
+	scanf("%d%d%d",&l,&n,&m);
+	arr[0] = 0;
+	for(int i=1; i<=n; i++) scanf("%d",&arr[i]);
+	sort(arr+1,arr+n+1);
+	for(int i=0; i<n; i++) diff[i] = arr[i+1] - arr[i];
+	diff[n] = l-arr[n];
+	printf("%d",binary_search(l));
 	return 0;
 }

677d19aae49e523d3233cae5dfb367e189ba9792
meskill <meskill@qq.com>
2019-12-10 22:13:43 +0800

    ABC#147-D-ac

--- Life In OJ/OJ's Solution/AtCoder/ABC#147/D.cpp
+++ Life In OJ/OJ's Solution/AtCoder/ABC#147/D.cpp
@@ -6,14 +6,22 @@ const int maxn = 3e5+100,MOD = 1e9+7;
 ll arr[maxn],bits[maxn][2],tt[maxn];
 
 int main() {
-    ll n,ans;
+    ll n,ans = 0;
     tt[0] = 1;
-    for(int i=0; i<=60; i++) {
-        tt[i] = tt[i]*2%MOD;
+    for(int i=1; i<=60; i++) {
+        tt[i] = tt[i-1]*2%MOD;
     }
     //slove()
     scanf("%lld",&n);
     for(int i=0; i<n; i++) scanf("%lld",&arr[i]);
+	for(int k=0; k<=60; k++) {
+		for(int i=0; i<n; i++) {
+			bits[k][arr[i]%2]++;
+			arr[i]/=2;
+		}
+		ans = (ans+bits[k][0]*bits[k][1]%MOD*tt[k]%MOD)%MOD;
+	}
+	ans %= MOD;
     printf("%lld\n",ans);
     return 0;
 }

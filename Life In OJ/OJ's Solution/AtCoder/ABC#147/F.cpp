0014450c7d5179268f45a004f6a605f3c2b34994
meskill <meskill@qq.com>
2019-12-17 17:48:28 +0800

    Atcoder-F-todo

---
+++ Life In OJ/OJ's Solution/AtCoder/ABC#147/F.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+const	int maxn = 2e5+100;
+struct SECTION {
+	int l,r;
+	SECTION() {}
+	SECTION(int l,int r):l(l),r(r) {}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	return 0;
+}

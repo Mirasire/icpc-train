aec76007e784a0f1661dbc9fc0ccfa6ce4a2d4ef
meskill <meskill@qq.com>
2019-12-23 16:36:36 +0800

    CodeForcesR#606_D_cpp_AC

--- Life In OJ/OJ's Solution/CodeForces/Round#606/D.cpp
+++ Life In OJ/OJ's Solution/CodeForces/Round#606/D.cpp
@@ -1,48 +1,63 @@
+//AC
 #include <bits/stdc++.h>
 using namespace std;
 
 const int maxn = 2e5+100;
 string str[maxn];
 map<string,bool> app;
-
-struct NODE {
-	int l,r;
-	NODE() {}
-	NODE(int l,int r):l(l),r(r) {}
-};
+vector<int> ans;
 
 int main() {
+	std::ios::sync_with_stdio(false);
 	int t,n;
-	string tmp;
+	int str01,str10,str00,str11;
 	cin >> t;
 	while(t--) {
 		app.clear();
+		ans.clear();
+		str10 = str01 = str00 = str11 = 0;
 		cin >> n;
-		int oo,ol,lo,ll;
-		vector<NODE> list;
-		oo = ol = lo = ll = 0;
-		for(int i=0; i<n; i++) {
+		for(int i=1; i<=n; i++) {
 			cin >> str[i];
+			if(str[i][0]=='0'&&str[i].back()=='1') str01++;
+			else if(str[i][0]=='1'&&str[i].back()=='0') str10++;
+			else if(str[i][0]=='0'&&str[i].back()=='0') str00++;
+			else if(str[i][0]=='1'&&str[i].back()=='1') str11++;
 			app[str[i]] = 1;
-			int c1 = str[0]-'0',c2 = str.back()-'0';
-			list.push_back(c1,c2);
-			if(!c1&&!c2) oo++;
-			else if(c1&&c2) ll++;
-			else if(c1) lo++;
-			else ol++;
 		}
-		if(n==1) cout << "0\n\n";
-		if(ll&&oo&&!lo&&!ol) cout << "-1\n";
-		else if(abs(ol-lo)==1) cout << "0\n\n";
-		else if(ol > lo) {
-			int miner = ol - lo - 1;
-			cout << miner << endl;
-			for(NODE now: list) {
-				if(now.l^now.r) {
-					string tstr = 
+		if(str00&&str11&&!str01&&!str10) {
+			cout << "-1\n";
+			continue;
+		} else if(abs(str10-str01)<=1) {
+			cout << "0\n";
+			continue;
+		} else if(str01 > str10 + 1) {
+			int res = (str10+str01)/2-str10;
+			cout << res << endl;
+			for(int i=1; res&&i<=n; i++) {
+				if(str[i][0]=='0'&&str[i].back()=='1') {
+					reverse(str[i].begin(),str[i].end());
+					if(!app[str[i]]) {
+						ans.push_back(i);
+						res--;
+					}
 				}
 			}
-		} else {
+			for(int idx: ans) cout << idx << " ";
+			cout << endl;
+		} else if(str10 > str01 + 1) {
+			int res = (str10+str01)/2-str01;
+			cout << res << endl;
+			for(int i=1; res&&i<=n; i++) {
+				if(str[i][0]=='1'&&str[i].back()=='0') {
+					reverse(str[i].begin(),str[i].end());
+					if(!app[str[i]]) {
+						ans.push_back(i);
+						res--;
+					}
+				}
+			}
+			for(int idx: ans) cout << idx << " ";
 		}
 	}
 	return 0;

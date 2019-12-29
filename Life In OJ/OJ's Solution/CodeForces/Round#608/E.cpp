0b6165b80a38346c187465cf76b54a18a68fbcd4
meskill <meskill@qq.com>
2019-12-29 22:32:16 +0800

    CB2109_init

--- Life In OJ/OJ's Solution/CodeForces/Round#608/E.cpp
+++ Life In OJ/OJ's Solution/CodeForces/Round#608/E.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long int ll;
 
+bool check(ll val) {
+}
+
+ll binary_search(ll l,ll r) {
+}
 
 int main() {
-	//std::ios::sync_with_stdio(false);
+	std::ios::sync_with_stdio(false);
+	ll n,k,r;
+	ll oddMax,evenMax;
+	cin >> n >> k;
+	if(n==1) return cout << 1 << endl,0;
+	oddMax = evenMax = 1;
+	//odd get max
+	//even get max
+	//cout << max(oddMax,evenMax) << endl;
 	return 0;
 }

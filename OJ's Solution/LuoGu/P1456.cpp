64741fecdb9985710dc521c67b5c2f6d0ec28533
meskill <meskill@qq.com>
2020-03-01 15:14:49 +0800

    P1456_AC

--- OJ's Solution/LuoGu/P1456.cpp
+++ OJ's Solution/LuoGu/P1456.cpp
@@ -21,7 +21,7 @@ int merge(int x,int y) {
 	if(!x || !y) return x|y;
 	if(tn[x].val < tn[y].val) swap(x,y);
 	rs(x) = merge(rs(x),y);
-	if(tn[ls(x)].d < tn[rs(x)].d) swap(ls(x),ls(y));
+	if(tn[ls(x)].d < tn[rs(x)].d) swap(ls(x),rs(x));
 	//tn[x].fa = tn[ls(x)].fa = tn[rs(x)].fa = x;
 	tn[x].d = tn[rs(x)].d+1;
 	return x;
@@ -31,23 +31,32 @@ int merge(int x,int y) {
 int main() {
 	std::ios::sync_with_stdio(false);
 	int n,m;
-	cin >> n >> m;
-	for(int i=1; i<=n; i++) tn[i].fa=i,cin >>tn[i].val;
-	int x,y,fx,fy;
-	while(m--) {
-		cin >> x >> y;
-		fx = find(x); fy = find(y);
-		if(fx==fy) cout << -1 << endl;
-		else {
-			tn[x].l = tn[x].r = 0;
-			tn[y].l = tn[y].r = 0;
-			int tx = tn[ls(fx)].fa = tn[rs(fx)].fa = merge(ls(fx),rs(fx));
-			int ty = tn[ls(fy)].fa = tn[rs(fy)].fa = merge(ls(fy),rs(fy));
-			tn[fx].val>>=1; tn[fy].val>>=1;
-			cout << max(tn[fx].val,tn[fy].val) << endl;
-			tn[tx].fa = tn[fx].fa = merge(tx,fx);
-			tn[ty].fa = tn[fy].fa = merge(ty,fy);
-			//merge
+	tn[0].d=-1;
+	while(cin >> n) {
+		for(int i=1; i<=n; i++) {
+			tn[i].fa=i;
+			tn[i].l=0;
+			tn[i].r=0;
+			tn[i].d=0;
+			cin >>tn[i].val;
+		}
+		cin >> m;
+		int x,y,fx,fy;
+		while(m--) {
+			cin >> x >> y;
+			fx = find(x); fy = find(y);
+			if(fx==fy) cout << -1 << endl;
+			else {
+				int tx = tn[ls(fx)].fa = tn[rs(fx)].fa = merge(ls(fx),rs(fx));
+				int ty = tn[ls(fy)].fa = tn[rs(fy)].fa = merge(ls(fy),rs(fy));
+				tn[fx].l = tn[fx].r = 0;
+				tn[fy].l = tn[fy].r = 0;
+				tn[fx].val>>=1; tn[fy].val>>=1;
+				int kx = tn[tx].fa = tn[fx].fa = merge(tx,fx);
+				int ky = tn[ty].fa = tn[fy].fa = merge(ty,fy);
+				int i = tn[kx].fa = tn[ky].fa = merge(kx,ky);
+				cout << tn[i].val << endl;
+			}
 		}
 	}
 	return 0;
